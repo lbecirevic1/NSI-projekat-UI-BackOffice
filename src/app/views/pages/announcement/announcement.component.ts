a4f7f1053a22adcced7ae2a8080a7c178d35591d
Medina Kapo <mkapo2@etf.unsa.ba>
2022-12-18 01:57:48 +0100

    minor changes

--- src/app/views/pages/announcement/announcement.component.ts
+++ src/app/views/pages/announcement/announcement.component.ts
@@ -1,44 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
 
 import { UtilioService} from "../../../service/utilio.service";
 import { Announcement} from "../../../models/announcement";
 
 
 @Component({
-  selector: 'app-announcement',
+  selector:    'app-announcement',
   templateUrl: './announcement.component.html',
   styleUrls: ['./announcement.component.scss']
 })
-export class AnnouncementComponent implements OnInit{
+export class AnnouncementComponent implements OnInit {
 
-  constructor(private service:UtilioService) { }
-  notifications:Announcement[]=[
-  ];
+  public notifications: Announcement[] = [];
 
   public liveDemoVisible = false;
   public editFormVisible = false;
 
-  public deleteItemId=0;
-  public editAnnouncement:any;
+  public deleteItemId = 0;
+  public editAnnouncement: any;
 
-  ngOnInit(){
-    this.service.getAnnouncements().subscribe(data=>{
+  constructor(private service: UtilioService) {}
 
-      for(let i=0;i<data.length;i++){
-        let notification=new Announcement(data[i].id,data[i].title,data[i].sourceUrl,data[i].description,data[i].content,data[i].publishDate,data[i].additionalInformation,
-          data[i].referenceStartDate,data[i].referenceEndDate)
+  ngOnInit() {
+    this.service.getAnnouncements().subscribe(data => {
+
+      for (let i = 0; i < data.length; i++) {
+        let notification = new Announcement(data[i].id, data[i].title, data[i].sourceUrl, data[i].description, data[i].content, data[i].publishDate, data[i].additionalInformation,data[i].referenceStartDate, data[i].referenceEndDate)
         this.notifications.push(notification);
-      }})
+      }
 
-  //  let nova=new Announcement(1,"test","test","test",'test',new Date(2022,12,17),"test",new Date(2022,12,17),new Date(2022,12,17));
-  ///this.notifications.push(nova);
+    })
+
+    //  let nova=new Announcement(1,"test","test","test",'test',new Date(2022,12,17),"test",new Date(2022,12,17),new Date(2022,12,17));
+    ///this.notifications.push(nova);
   }
 
 
-  toggleDeleteButton(itemId:number) {
+  toggleDeleteButton(itemId: number) {
     this.liveDemoVisible = !this.liveDemoVisible;
-    this.deleteItemId=itemId;
+    this.deleteItemId = itemId;
   }
 
   closeDeleteButton() {
@@ -48,23 +48,24 @@ export class AnnouncementComponent implements OnInit{
   handleLiveDemoChange(event: boolean) {
     this.liveDemoVisible = event;
   }
-  toggleEditButtonDemo(item:any){
+
+  toggleEditButtonDemo(item: any) {
     this.toggleEditDemo();
-    this.editAnnouncement=item;
+    this.editAnnouncement = item;
   }
-  toggleEditDemo(){
+
+  toggleEditDemo() {
     this.editFormVisible = !this.editFormVisible;
 
   }
 
-  handleEditModalChange(event: boolean){
+  handleEditModalChange(event: boolean) {
     this.editFormVisible = event;
   }
 
-  refreshAnnouncements(){
-    this.service.getAnnouncements().subscribe(data=>{
-      console.log(data[0]);
-      for(let i=0;i<data.length;i++){
+  refreshAnnouncements() {
+    this.service.getAnnouncements().subscribe(data => {
+      for (let i = 0; i < data.length; i++) {
         this.notifications.push(data[i]);
       }
     })
@@ -76,7 +77,4 @@ export class AnnouncementComponent implements OnInit{
     this.liveDemoVisible = !this.liveDemoVisible;
   }
 
-  editItem(){
-    //to do
-  }
 }
