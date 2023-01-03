6e2c095561cfa04a2c5b8070f929c100ff9df61f
ahalilovi15 <ahalilovi15@etf.unsa.ba>
2023-01-03 21:04:21 +0100

    sorting done

--- src/app/views/logs/logs.component.ts
+++ src/app/views/logs/logs.component.ts
@@ -9,47 +9,57 @@ import { UtilioService } from '../../service/utilio.service';
   templateUrl: './logs.component.html',
   styleUrls: ['./logs.component.scss'],
 })
-
 export class LogsComponent implements OnInit {
   public logs: LogWithTime[] = [];
   public paging: Paging = {};
   public pages: number[] = [];
-  public providers: {name: string, id: number}[] = [];
+  public providers: { name: string; id: number }[] = [];
   public providerId: number = -1;
   public status: boolean | number = -1;
   public recordsPerPage: number = 5;
   public logParameters: any = {};
   public sortCriteria: any = [];
+  public reqStartDateAsc: number = 0;
   public asc: number = 0;
+  public reqEndDateAsc: number = 0;
+  public providerNameAsc: number = 0;
   public cleared: boolean = false;
   constructor(private service: UtilioService) {}
 
   createLogsArray(data: LogsResponse) {
-    return data.data?.map((log: Log) => {return {
-      id: log.id,
-      requestStartDate: dayjs(log.requestStartTime).format("MMM D, YYYY"),
-      requestStartTime: dayjs(log.requestStartTime).format("HH:mm:ss"),
-      requestEndDate: dayjs(log.requestEndTime).format("MMM D, YYYY"),
-      requestEndTime: dayjs(log.requestEndTime).format("HH:mm:ss"),
-      status: log.success ? 'Success' : 'Failure',
-      providerName: log.providerName,
-      providerId: log.providerId,
-    }});
+    return data.data?.map((log: Log) => {
+      return {
+        id: log.id,
+        requestStartDate: dayjs(log.requestStartTime).format('MMM D, YYYY'),
+        requestStartTime: dayjs(log.requestStartTime).format('HH:mm:ss'),
+        requestEndDate: dayjs(log.requestEndTime).format('MMM D, YYYY'),
+        requestEndTime: dayjs(log.requestEndTime).format('HH:mm:ss'),
+        status: log.success ? 'Success' : 'Failure',
+        providerName: log.providerName,
+        providerId: log.providerId,
+      };
+    });
   }
 
   setLogs(page: number) {
-    this.service.getLogs(page, this.recordsPerPage, this.logParameters, this.sortCriteria).subscribe((data: LogsResponse) => {
-      this.logs = this.createLogsArray(data);
-      this.paging = data.paging;
-      this.pages = Array.from({length: data.paging.pages || 1}, (value, key) => key + 1)
-
-    });
+    this.service
+      .getLogs(page, this.recordsPerPage, this.logParameters, this.sortCriteria)
+      .subscribe((data: LogsResponse) => {
+        this.logs = this.createLogsArray(data);
+        this.paging = data.paging;
+        this.pages = Array.from(
+          { length: data.paging.pages || 1 },
+          (value, key) => key + 1
+        );
+      });
   }
   ngOnInit() {
     this.setLogs(1);
-    this.service.getProviders().subscribe(data=>{
-     this.providers = data?.map((provider: any) => {return {id: provider.id, name: provider.name}});
-    })
+    this.service.getProviders().subscribe((data) => {
+      this.providers = data?.map((provider: any) => {
+        return { id: provider.id, name: provider.name };
+      });
+    });
   }
 
   onPageChange(page: number) {
@@ -58,25 +68,35 @@ export class LogsComponent implements OnInit {
   }
 
   onPageChangeNext(next: boolean) {
-    next && this.paging.page ? this.paging.page += 1 : this.paging.page! -= 1;
+    next && this.paging.page
+      ? (this.paging.page += 1)
+      : (this.paging.page! -= 1);
     this.setLogs(this.paging.page!);
   }
 
   onChange(event: any, isProvider: boolean) {
     this.cleared = false;
-    isProvider ? this.providerId = Number(event.target.value) : 
-    this.status = event.target.value === 'true' ? true :  
-    event.target.value === 'false' ? false : -1;
+    isProvider
+      ? (this.providerId = Number(event.target.value))
+      : (this.status =
+          event.target.value === 'true'
+            ? true
+            : event.target.value === 'false'
+            ? false
+            : -1);
 
-    this.logParameters = {}
-    this.providerId !== -1 && event.target.value !== -1 && Object.assign(this.logParameters, {providerId: this.providerId});
-    this.status !== -1 && Object.assign(this.logParameters, {success: this.status});
+    this.logParameters = {};
+    this.providerId !== -1 &&
+      event.target.value !== -1 &&
+      Object.assign(this.logParameters, { providerId: this.providerId });
+    this.status !== -1 &&
+      Object.assign(this.logParameters, { success: this.status });
     this.setLogs(this.paging.page!);
   }
 
   onChangeNumberOfRows(event: any) {
     this.recordsPerPage = Number(event.target.value);
-    this.setLogs(this.paging.page!)
+    this.setLogs(this.paging.page!);
   }
 
   onClearFilter() {
@@ -85,12 +105,47 @@ export class LogsComponent implements OnInit {
     this.setLogs(this.paging.page!);
   }
 
+  handleOrder(name: string) {
+    switch (name) {
+      case 'RequestStartTime':
+        this.reqStartDateAsc = !!this.reqStartDateAsc ? 0 : 1;
+        this.reqEndDateAsc = 0;
+        this.providerNameAsc = 0;
+        return this.reqStartDateAsc;
+      case 'RequestEndTime':
+        this.reqEndDateAsc = !!this.reqEndDateAsc ? 0 : 1;
+        this.providerNameAsc = 0;
+        this.reqStartDateAsc = 0;
+        return this.reqEndDateAsc;
+      case 'Provider.Name':
+        this.providerNameAsc = !!this.providerNameAsc ? 0 : 1;
+        this.reqStartDateAsc = 0;
+        this.reqEndDateAsc = 0;
+        return this.providerNameAsc;
+      default:
+        return 0;
+    }
+  }
+
+  onSort(name: string) {
+    this.sortCriteria = [
+      {
+        column: name,
+        order: this.handleOrder(name),
+        priority: 0,
+      },
+    ];
+
+    this.setLogs(this.paging.page!);
+  }
   onSortColumn(name: string) {
-    this.sortCriteria = [{
-      column: name,
-      order: this.asc,
-      priority: 0
-    }]
+    this.sortCriteria = [
+      {
+        column: name,
+        order: this.asc,
+        priority: 0,
+      },
+    ];
 
     this.setLogs(this.paging.page!);
     this.asc = !!this.asc ? 0 : 1;
