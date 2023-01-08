c2e4566e501df1a6e163ae8468cd09b71178489b
Kenan Omic <kenanomic@192.168.1.39>
2023-01-08 01:14:02 +0100

    Changed headline in route

--- src/app/views/handle-announcement/handle-announcement-router.module.ts
+++ src/app/views/handle-announcement/handle-announcement-router.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
         path: '',
         component: AnnouncementHandlerComponent,
         data: {
-            title: 'Overview',
+            title: 'Announcement Handling',
         },
     },
 ];
